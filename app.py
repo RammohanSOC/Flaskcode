5f72bdffc23e6322b202d571d73df44263d09c41
RammohanSOC <abburirammohan2@gmail.com>
2026-06-24 16:54:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask
 import os
 
 app = Flask(__name__)
@@ -6,8 +6,7 @@ app.secret_key = os.environ.get('SECRET_KEY')
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return 'ISS Technologies - Website is Live!'
 
 if __name__ == '__main__':
     app.run()
-
